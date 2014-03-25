a8a3c0dd38d2b74e157e46dc5cff7584760e5f8d
Andy Gardner <andy.gardner@fundamental.net>
2014-03-25 11:15:17 +0200

    Added support for resuming downloads cleaning up whitespace to force new travis build

--- mega/mega.py
+++ mega/mega.py
@@ -573,7 +573,7 @@ class Mega(object):
         file_mac = str_to_a32(mac_str)
 
         temp_output_file.close()
-        
+
         # check mac integrity
         if (file_mac[0] ^ file_mac[1], file_mac[2] ^ file_mac[3]) != meta_mac:
             raise ValueError('Mismatched mac')
